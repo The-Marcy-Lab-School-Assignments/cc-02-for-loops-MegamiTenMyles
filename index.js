00b672b6d4d31f71b27ba6328f50a710b6f73bc0
Myles <mylesmatthews2112@gmail.com>
2021-09-29 14:29:07 +0000

    1-5 DONE!

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 //1
 function countToTen(){
-  
+  for(let i = 1; i <= 10; i++){
+      console.log(i)
+  }
 }
-
+//countToTen()
 //2
-function countFromOne(){
-  
+function countFromOne(number){
+  for(let i = 1; i <= number; i++){
+      console.log(i)
+  }
 }
-
+//countFromOne(29)
 //3
-function countEveryEven(){
-  
-}
+function countEveryEven(number){
+    for(let i = 1; i <= number; i++){
+      if (i % 2 === 0){
+          console.log(i)
+      }
+  }
+  }
+countEveryEven(23)
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(number){
+   for(let i = 1; i <= number; i++){
+      if (i % 2 != 0){
+          console.log(i)
+      }
+  }
 }
-
+//countEveryOdd(23)
 //5 
-function countEvens(){
+function countEvens(numbers){
+    const evens = numbers.filter(x => x % 2 === 0)
+    return evens
+    
   
 }
-
+console.log(countEvens([0, 10, 23, 41, 22, 76]))
